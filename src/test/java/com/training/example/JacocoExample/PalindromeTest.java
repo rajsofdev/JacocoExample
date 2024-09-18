5479c775e2fecf3e0579f4e79421be58900d2be3
Rambabu <rkrambabu3@gmail.com>
2024-09-18 17:01:48 +0530

    modified the java file object

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class PalindromeTest {
 	@Test
 	public void test1() {
-		CompareString obj1=new Palindrome();
+		Palindrome obj1=new Palindrome();
 		obj1.containsOrNot("Seleniam is Test Automation Tool");
 	}
 
